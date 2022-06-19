62e6e0a7c3fcf606c8b7439cc1f6963e0a1f3adc
nagudeloiud <noberto.agudelo@est.iudigital.edu.co>
2022-06-18 23:51:17 -0500

    :sparkles: solution to problems of cors

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 
 const fileUpload = require('express-fileupload');
-
+const cors = require('cors');
 
 const tiposEquipo = require('./routes/tipoEquipo');
 const estados = require('./routes/estado');
 const usuarios = require('./routes/usuario');
 const marcas = require('./routes/marca');
 const inventarios = require('./routes/inventario');
+
 
 //middlewares
 app.use(express.urlencoded({extended: false}));
@@ -19,6 +20,11 @@ app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
 }));
+app.use(cors());
+/* app.use(cors(
+    config.application.cors.server
+  )); */
+
 
 app.use('/api/usuarios', usuarios);
 app.use('/api/tiposequipo', tiposEquipo);
